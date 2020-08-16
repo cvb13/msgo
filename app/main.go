e134ab50c4997338156ea71ee03bfed922c285df
Cesar  Vargas Bolaños <cesar.vargas@Cesar-Vargas-MacBook-Pro.local>
2020-08-16 16:22:23 -0600

    Generating hash using URL, method and body. Pending to accept embedded json nodes

--- app/main.go
+++ app/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -25,6 +27,8 @@ func AddMockHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	fmt.Printf("Hash: %x\n", newMock.hash())
+
 	if exists(newMock) {
 		http.Error(w, "Duplicated", http.StatusConflict)
 		return
@@ -40,21 +44,31 @@ func AddMockHandler(w http.ResponseWriter, r *http.Request) {
 //Handler to dynamic handling new requests
 func DynamicMockHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var mock RequestMock
-	mock.URL = r.URL.Path
+
+	reqBody, err := decodeBody(w, r)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	mock := RequestMock{URL: r.URL.Path, RequestMethod: r.Method, RequestBody: reqBody}
+	fmt.Printf("Mock:%+v\n", &mock)
+	fmt.Printf("ExpectedHash:%x\n", mock.hash())
 
 	if !exists(mock) {
 		http.Error(w, "Not Found", http.StatusNotFound)
 		return
 	}
 
-	mock = getRequestMock(mock.URL)
+	mock = getRequestMock(mock.Hash)
 	fmt.Printf("ResponseBody:%s\n", mock.ResponseBody)
 	w.Write([]byte(mock.ResponseBody))
 }
 
 //RequestMock struct and functions
 type RequestMock struct {
+	Hash            [32]byte
 	URL             string   `json:"url"`
 	RequestBody     string   `json:"requestBody"`
 	RequestMethod   string   `json:"requestMethod"`
@@ -67,19 +81,65 @@ type RequestMock struct {
 
 func exists(mock RequestMock) bool {
 	for i := range mocks {
-		if mock.URL == mocks[i].URL {
+		if mock.Hash == mocks[i].Hash {
 			return true
 		}
 	}
 	return false
 }
 
-func getRequestMock(url string) RequestMock {
+func getRequestMock(hash [32]byte) RequestMock {
 	var result RequestMock
 	for i := range mocks {
-		if url == mocks[i].URL {
+		if hash == mocks[i].Hash {
 			result = mocks[i]
 		}
 	}
 	return result
+}
+
+// Generates hash based on request URL, Method and Body
+func (r *RequestMock) hash() [32]byte {
+	var buffer bytes.Buffer
+
+	//URL + Method + Body
+	buffer.WriteString(r.URL)
+	buffer.WriteString(r.RequestMethod)
+	buffer.WriteString(r.RequestBody)
+	generatedHash := sha256.Sum256([]byte(buffer.String()))
+
+	r.Hash = generatedHash
+	return generatedHash
+}
+
+// Decodes the body in a string representing the json values. Used to calculate the hash
+func decodeBody(w http.ResponseWriter, r *http.Request) (string, error) {
+	decoder := json.NewDecoder(r.Body)
+	reqBody := map[string]interface{}{}
+	err := decoder.Decode(&reqBody)
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Printf("ReqBody:%+v\n", &reqBody)
+
+	var result string
+	for key, val := range reqBody {
+		switch v := val.(type) {
+		default:
+			fmt.Printf("\nunexpected type %T", v)
+		case int:
+			n := fmt.Sprintf("{\"%v\":%v}", key, val)
+			result = result + n
+		case float64:
+			n := fmt.Sprintf("{\"%v\":%v}", key, val)
+			result = result + n
+		case string:
+			n := fmt.Sprintf("{\"%v\":\"%v\"}", key, val)
+			result = result + n
+		}
+
+	}
+	return result, nil
+
 }
