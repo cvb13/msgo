9879065bfe7a871bbcfccea2247498a4e0856d92
Cesar  Vargas Bolaños <cesar.vargas@Cesar-Vargas-MacBook-Pro.local>
2020-08-17 19:09:08 -0600

    Moving struct to a different file

--- app/main.go
+++ app/main.go
@@ -11,19 +11,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-//RequestMock struct and functions
-type RequestMock struct {
-	Hash            [32]byte          `json:"Hash"`
-	URL             string            `json:"url"`
-	RequestBody     string            `json:"requestBody"`
-	RequestMethod   string            `json:"requestMethod"`
-	RequestHeaders  map[string]string `json:"requestHeaders"`
-	ResponseBody    string            `json:"responseBody"`
-	ResponseCode    int               `json:"responseCode"`
-	ResponseHeaders map[string]string `json:"responseHeaders"`
-	Override        bool              `json:"override"`
-}
-
 var router = mux.NewRouter()
 var mocks []RequestMock
 
